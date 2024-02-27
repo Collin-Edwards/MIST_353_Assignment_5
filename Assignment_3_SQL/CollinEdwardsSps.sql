d64b24edb91f60cc33b8dd0c25338ad612391569
Collin Edwards <edwardscollin35@yahoo.com>
2024-02-27 15:49:39 -0500

    Update CollinEdwardsSps.sql

--- Assignment_3_SQL/CollinEdwardsSps.sql
+++ Assignment_3_SQL/CollinEdwardsSps.sql
@@ -2,12 +2,16 @@ create proc registeruser
     @username nvarchar(50),
     @passwordhash nvarchar(255),
     @email nvarchar(100),
-    @isdoduser bit
+    @isdoduser bit,
+    @firstname nvarchar(50),
+    @lastname nvarchar(50)
 as
 begin
-    insert into users (username, passwordhash, email, isdoduser)
-    values (@username, @passwordhash, @email, @isdoduser);
+    insert into users (username, passwordhash, email, isdoduser, firstname, lastname)
+    values (@username, @passwordhash, @email, @isdoduser, @firstname, @lastname);
 end;
+go
+
 go
 create proc validateuserlogin
     @username nvarchar(50),
