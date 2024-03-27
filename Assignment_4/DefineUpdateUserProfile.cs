7db0f1fe1025b8cdb1433c021aaa32628bc82dbd
LandenRiggleman <159197111+LandenRiggleman@users.noreply.github.com>
2024-03-26 23:56:42 -0400

    Create DefineUpdateUserProfile.cs
    Defines User Profile information

---
+++ Assignment_4/DefineUpdateUserProfile.cs
@@ -0,0 +1,7 @@
+public class UserProfile
+{
+    public int UserID { get; set; }
+    public string NewPasswordHash { get; set; }
+    public string NewEmail { get; set; }
+    public bool ChangeDODStatus { get; set; }
+}
